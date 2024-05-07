56a3bad8c5fe6d3cd79a04e6c4264a6bf1728b94
m1m1sha <18262227804@163.com>
2024-05-07 18:54:41 +0800

    🐳 chore: set path

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -4,6 +4,7 @@ import { cn } from './cn'
 
 export default defineConfig({
   title: 'Easytier',
+  base: '/easytier-doc/',
   locales: {
     root: { label: '简体中文', ...cn },
     en: { label: 'English', ...en },
