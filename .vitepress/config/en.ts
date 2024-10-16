4a2fba61cf40cc78c39711bd0ee4c99c9be82a2f
fanyang89 <fanyang89@outlook.com>
2024-10-16 13:02:41 +0800

    Fix letter cases and add space between words

--- .vitepress/config/en.ts
+++ .vitepress/config/en.ts
@@ -2,34 +2,34 @@ import { defineConfig } from 'vitepress'
 
 export const en = defineConfig({
   lang: 'en',
-  description: 'a simple, safe and decentralized VPN networking solution implemented with the Rust language and Tokio framework.',
+  description: 'A simple, safe and decentralized VPN networking solution implemented with the Rust language and Tokio framework',
 
   themeConfig: {
     sidebar: [
       {
-        text: 'Getting Started',
+        text: 'Getting started',
         items: [
           { text: 'Introduction', link: '/en/guide/introduction' },
-          { text: 'installation', link: '/en/guide/installation' },
+          { text: 'Installation', link: '/en/guide/installation' },
         ],
       },
       {
         text: 'Networking',
         link: '/en/guide/networking',
         items: [
-          { text: 'Two Node', link: '/en/guide/network/two-node-networking' },
-          { text: 'Multi Node', link: '/en/guide/network/multi-node-networking' },
-          { text: 'Subnet Proxy', link: '/en/guide/network/point-to-networking' },
-          { text: 'Network to Network', link: '/en/guide/network/network-to-network' },
-          { text: 'Without Public IP', link: '/en/guide/network/networking-without-public-ip' },
+          { text: 'Peer to peer networking', link: '/en/guide/network/two-node-networking' },
+          { text: 'Multi peers networking', link: '/en/guide/network/multi-node-networking' },
+          { text: 'Subnet proxy', link: '/en/guide/network/point-to-networking' },
+          { text: 'Network to network', link: '/en/guide/network/network-to-network' },
+          { text: 'Without public IP', link: '/en/guide/network/networking-without-public-ip' },
           { text: 'Use WireGuard client', link: '/en/guide/network/use-easytier-with-wireguard-client' },
-          { text: 'SOCKS5', link: '/en/guide/network/socks5' },
-          { text: 'No TUN Mode (No Root Permission Required', link: '/en/guide/network/no-root' },
-          { text: 'Self-Hosted Public Server', link: '/en/guide/network/host-public-server' },
-          { text: 'Installing as a Windows Service (Auto-Start on Boot)', link: '/en/guide/network/install-as-a-windows-service' },
-          { text: 'Installing as a Linux Systemd Service', link: '/en/guide/network/install-as-a-systemd-service' },
-          { text: 'Other Configurations', link: '/en/guide/network/configurations' },
-          { text: 'Configuration File', link: '/en/guide/network/config-file' },
+          { text: 'Socks5', link: '/en/guide/network/socks5' },
+          { text: 'No TUN mode (rootless)', link: '/en/guide/network/no-root' },
+          { text: 'Self-hosted public server', link: '/en/guide/network/host-public-server' },
+          { text: 'Installing as a Windows service (Auto start on boot)', link: '/en/guide/network/install-as-a-windows-service' },
+          { text: 'Installing as a Linux systemd service', link: '/en/guide/network/install-as-a-systemd-service' },
+          { text: 'Other configurations', link: '/en/guide/network/configurations' },
+          { text: 'Configuration file', link: '/en/guide/network/config-file' },
         ],
       },
       {
