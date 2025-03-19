6a18a8f26513c63198fadda783d69c0ef007518b
Sijie.Sun <sunsijie@buaa.edu.cn>
2025-03-19 17:31:59 +0800

    Update metadata.data.ts

--- metadata.data.ts
+++ metadata.data.ts
@@ -1,7 +1,7 @@
 export default {
   load() {
     return {
-      easytier_latest_version: '2.2.3',
+      easytier_latest_version: '2.2.4',
       github_accels: [
         'https://ghfast.top/',
       ],
