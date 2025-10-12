35ce4471468f792068b4bce5e7570f77205ec953
PurifierProbe <88586891+purifierprobe@users.noreply.github.com>
2025-10-12 20:52:25 +0800

    更新 关于 P2P 目录

--- .vitepress/config/cn.ts
+++ .vitepress/config/cn.ts
@@ -62,6 +62,7 @@ export const cn = defineConfig({
       },
       {
         items: [
+          { text: '关于 P2P', link: '/guide/aboutp2p'}
           { text: '性能测试', link: '/guide/perf' },
           { text: '路线图', link: '/guide/roadmap' },
           { text: '社区和贡献', link: '/guide/community-and-contribution' },
