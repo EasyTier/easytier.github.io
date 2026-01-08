6333753976ebc5992190ea7b5142ec69c4ed1515
rinne <180847792+rinnein@users.noreply.github.com>
2026-01-08 13:46:19 +0800

    更新 GitHub 加速器列表，添加新代理地址

--- metadata.data.ts
+++ metadata.data.ts
@@ -4,6 +4,10 @@ export default {
       easytier_latest_version: '2.4.5',
       github_accels: [
         'https://ghfast.top/',
+        'https://v6.gh-proxy.org/',
+        'https://hk.gh-proxy.org/',
+        'https://cdn.gh-proxy.org/',
+        'https://edgeone.gh-proxy.org/',
       ],
     }
   },
